1f54d7c5a457b3cfcc72e7e1310132d8b9f485de
Sergey Utkin <utkins01@gmail.com>
2019-01-16 07:40:39 +0300

    16.01.2019
    Signed-off-by: Sergey Utkin <utkins01@gmail.com>

--- lib/pyrmalib/access.py
+++ lib/pyrmalib/access.py
@@ -14,6 +14,9 @@
    limitations under the License.
 """
 
+from pyrmalib import schema, error
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
 """
 Модуль контроля и проверки правил доступа.
 Режимы(приоритет от наименьшего к наибольшему.):
@@ -25,8 +28,6 @@
     6.  Пользователь -> Группа хостов
     7.  Пользователь -> Хост
 """
-from pyrmalib import schema, error
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 user_access_map = {
     'ShowHostInformation': 0,       # Просмотр информации об узле, автоматом отключает
@@ -52,6 +53,7 @@ connection_access_map = {
 
 def get_permission(engine, subj, t_subj=0, obj=None, t_obj=None):
     # Пользователь -> Хост
+    global permission
     if t_subj == 0 and t_obj == 2:
         with schema.db_select(engine) as db:
             try:
@@ -59,24 +61,80 @@ def get_permission(engine, subj, t_subj=0, obj=None, t_obj=None):
                     .filter(schema.Permission.subject == subj) \
                     .filter(schema.Permission.t_subject == t_subj) \
                     .filter(schema.Permission.object == obj) \
-                    .filter(schema.Permission.t_object == t_obj)\
+                    .filter(schema.Permission.t_object == t_obj) \
                     .one()
                 return permission
             except NoResultFound:
                 pass
             except MultipleResultsFound:
                 raise error.QueryError("Multiple Results Found schema.Permission: U: {0} H: {1}".format(subj, obj))
+
     # Пользователь -> Группа хостов
     if t_subj == 0 and (t_obj == 2 or t_obj == 1):
+        with schema.db_select(engine) as db:
+            try:
+                host_groups = db.query(schema.GroupHost.group) \
+                    .filter(schema.GroupHost.host == obj) \
+                    .group_by(schema.GroupHost.group).subquery()
 
-        pass
+                permission = db.query(schema.Permission) \
+                    .filter(schema.Permission.t_object == 2) \
+                    .filter(schema.Permission.object.in_(host_groups)) \
+                    .filter(schema.Permission.t_subject == t_subj) \
+                    .filter(schema.Permission.subject == subj).all()
+            except NoResultFound:
+                pass
+
+            if len(permission) != 0:
+                return permission
+
     # Группа пользователей -> Хост
     if (t_subj == 0 or t_subj == 1) and t_obj == 2:
+        with schema.db_select(engine) as db:
+            try:
+                user_groups = db.query(schema.GroupUser.group) \
+                    .filter(schema.GroupUser.user == subj) \
+                    .group_by(schema.GroupUser.group).subquery()
+
+                permission = db.query(schema.Permission) \
+                    .filter(schema.Permission.t_object == t_obj) \
+                    .filter(schema.Permission.subject.in_(user_groups)) \
+                    .filter(schema.Permission.t_subject == 1) \
+                    .filter(schema.Permission.subject == subj).all()
+            except NoResultFound:
+                pass
+
+            if len(permission) != 0:
+                return permission
         pass
     # Группа пользователей -> Група хостов
     if (t_subj == 0 or t_subj == 1) and (t_obj == 2 or t_obj == 1):
+        with schema.db_select(engine) as db:
+            try:
+                user_groups = db.query(schema.GroupUser.group) \
+                    .filter(schema.GroupUser.user == subj) \
+                    .group_by(schema.GroupUser.group).subquery()
+
+                host_groups = db.query(schema.GroupHost.group) \
+                    .filter(schema.GroupHost.host == obj) \
+                    .group_by(schema.GroupHost.group).subquery()
+
+                permission = db.query(schema.Permission) \
+                    .filter(schema.Permission.t_object == 1) \
+                    .filter(schema.Permission.t_subject == 1) \
+                    .filter(schema.Permission.subject.in_(user_groups)) \
+                    .filter(schema.Permission.object.in_(host_groups)) \
+                    .filter(schema.Permission.subject == subj).all()
+            except NoResultFound:
+                pass
+
+            if len(permission) != 0:
+                return permission
         pass
+
     # Общий режим пользователя
+    # Общий режим группы хостов
+    # Общий режим группы пользователей
     with schema.db_select(engine) as db:
         try:
             permission = db.query(schema.Permission) \
@@ -89,8 +147,17 @@ def get_permission(engine, subj, t_subj=0, obj=None, t_obj=None):
         except MultipleResultsFound:
             raise error.QueryError("Multiple Results Found schema.Permission: U: {0}".format(subj))
 
-    # Общий режим группы хостов
-    # Общий режим группы пользователей
+    with schema.db_select(engine) as db:
+        try:
+            permission = db.query(schema.Permission) \
+                .filter(schema.Permission.subject == subj) \
+                .filter(schema.Permission.t_subject == t_subj) \
+                .one()
+            return permission
+        except NoResultFound:
+            pass
+        except MultipleResultsFound:
+            raise error.QueryError("Multiple Results Found schema.Permission: U: {0}".format(subj))
 
     return False
 
@@ -156,9 +223,19 @@ def user_check_access(engine, t_subj, subj, access, t_obj=None, obj=None):
     :return: bool
     """
 
-    permission = get_permission(engine, subj, t_subj=t_subj, obj=obj, t_obj=t_obj)
+    perm = get_permission(engine, subj, t_subj=t_subj, obj=obj, t_obj=t_obj)
 
-    return UserAccess(permission.user_access).get(access)
+    if isinstance(perm, list):
+        for i in perm:
+            if UserAccess(i.user_access).get(access):
+                return True
+        return False
+    elif isinstance(perm, schema.Permission):
+        return UserAccess(perm.user_access).get(access)
+    elif not perm:
+        return False
+    else:
+        raise TypeError('user_check_access: value did not schema.Permission')
 
 
 def change_access(engine, user_id, access, set_access=False):
